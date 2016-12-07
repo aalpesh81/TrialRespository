1c2403290db0cd7da47f244900fb4ce47b399e88
aalpesh81 <alpesh_savani@yahoo.co.in>
2016-12-07 16:16:42 +0000

    This is My Second Change in Code

--- HelloWord/src/HelloWord.java
+++ HelloWord/src/HelloWord.java
@@ -4,7 +4,8 @@
 public class HelloWord {
 
     public static void main(String args[]) {
-        System.out.println("Hello World");
-        System.out.println("This is my very Fisrt JAVA Projcet.");
+        System.out.println("\"Hello World\"");
+        System.out.println("\"This is my very Fisrt JAVA Projcet.\"");
+        System.out.println("\"Eat Java, Drink Java, Think Java, Sleep Java\"");
     }
 }
